6dff9b8ed2eccbb2985594529d9bb1bb5af263cc
shobhit19 <rashushobhitvarshney@gmail.com>
2020-04-19 23:18:43 +0530

    added changes to hide warnings

--- src/main/java/com/qa/flipkart/testbase/TestBase.java
+++ src/main/java/com/qa/flipkart/testbase/TestBase.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
@@ -76,6 +77,7 @@ public class TestBase {
 	}
 
 	public static void launchUrl() {
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 		driver.get(prop.getProperty("url"));
 	}
 }
